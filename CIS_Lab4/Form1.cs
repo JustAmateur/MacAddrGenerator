ccf04752ab258995cf1b8241926affa819dcfdc3
Vyacheslav Kuskov <vyacheslav@MacBook-Pro-Vyacheslav.local>
2018-02-19 21:36:53 +0300

    First upload

---
+++ CIS_Lab4/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+using NpgsqlTypes;
+
+namespace CIS_Lab4
+{
+    public partial class Form1 : Form
+    {
+        public string strconn;
+        public NpgsqlConnection cnn = new NpgsqlConnection();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void запросСПараметромToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ConnectionUpdate();
+            cnn.ConnectionString = strconn;
+                cnn.Open();
+                string stsql = "";
+                using (NpgsqlCommand myCommand = new NpgsqlCommand("select * from \"Жанры\" where \"Имя\" like :value1", cnn))
+                {
+                    myCommand.Parameters.Add(new NpgsqlParameter("value1",
+                        NpgsqlTypes.NpgsqlDbType.Varchar));
+                    stsql = "%" + commandTextBox.Text + "%";
+                    myCommand.Parameters[0].Value = stsql;
+                    using (NpgsqlDataReader dr = myCommand.ExecuteReader())
+                    {
+                        commandListBox.Items.Clear();
+                        while (dr.Read())
+                        {
+                            commandListBox.Items.Add(Convert.ToString(dr[1]));
+                        }
+                    }
+                }
+                cnn.Close();
+            }
+
+        public void ConnectionUpdate()
+        {
+            strconn = string.Format("Server={0};Port={1};UserID={2};Password={3};Database={4}",
+                serverTextBox.Text,
+                portTextBox.Text,
+                userTextBox.Text,
+                passwordTextBox.Text,
+                baseTextBox.Text);
+        }
+
+        private void запросБезПараметраToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ConnectionUpdate();
+            cnn.ConnectionString = strconn;
+            cnn.Open();
+            string stsql = "";
+            stsql = "select * from \"Жанры\" where \"Имя\" like \'%" +
+                    commandTextBox.Text +
+                    "%\';";
+            using (NpgsqlCommand myCommand = new NpgsqlCommand(stsql, cnn))
+            {
+                using (NpgsqlDataReader dr = myCommand.ExecuteReader())
+                {
+                    commandListBox.Items.Clear();
+                    while (dr.Read())
+                    {
+                        commandListBox.Items.Add(Convert.ToString(dr[1]));
+                    }
+                }
+            }
+            cnn.Close();
+
+        }
+
+        private void скалярнаяФункцияСПараметромToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ConnectionUpdate();
+            cnn.ConnectionString = strconn;
+            cnn.Open();
+            try
+            {
+                NpgsqlCommand myCommand = new NpgsqlCommand("myfunc", cnn);
+                myCommand.CommandType = CommandType.StoredProcedure;
+
+                myCommand.Parameters.Add(new NpgsqlParameter());
+                myCommand.Parameters[0].NpgsqlDbType = 
+                    NpgsqlTypes.NpgsqlDbType.Integer;
+                myCommand.Parameters[0].Value = 4;
+
+                myCommand.Parameters.Add(new NpgsqlParameter());
+                myCommand.Parameters[1].NpgsqlDbType =
+                    NpgsqlTypes.NpgsqlDbType.Integer;
+                myCommand.Parameters[1].Value = 5;
+
+                myCommand.Parameters.Add(new NpgsqlParameter());
+                myCommand.Parameters[2].NpgsqlDbType =
+                    NpgsqlTypes.NpgsqlDbType.Integer;
+                myCommand.Parameters[2].Direction =
+                    ParameterDirection.Output;
+
+                var result = myCommand.ExecuteScalar();
+                MessageBox.Show(Convert.ToString(myCommand.Parameters[2].Value));
+            }
+            finally
+            {
+                cnn.Close();
+            }
+        }
+
+        private void генерацияMacадресовToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ConnectionUpdate();
+            cnn.ConnectionString = strconn;
+            cnn.Open();
+            try
+            {
+                NpgsqlCommand myCommand = new NpgsqlCommand();
+                myCommand.Connection = cnn;
+                myCommand.CommandText =
+                    "INSERT INTO mac_address(value) SELECT * FROM generate_random_mac(\'" +
+                    MacGenerator.GenerateFirstOctet(maskTextBox.Text).ToLower() + "\'," +
+                    Convert.ToInt32(commandTextBox.Text) + ") AS (a macaddr);";
+                //NpgsqlParameter parameter = new NpgsqlParameter();
+                //parameter.ParameterName = "address";
+                //myCommand.CommandText =
+                //"INSERT INTO mac_address(value) VALUES (@address)";
+                //myCommand.Parameters.Add(parameter);
+                //for (int i = 0; i < Convert.ToInt32(commandTextBox.Text); i++)
+                //{
+
+                //myCommand.Parameters[0].Value = MacGenerator.GenerateAddress();
+                myCommand.ExecuteNonQuery();
+                //}
+
+                MessageBox.Show("Генерация выполнена успешно!",
+                    "Успех",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                throw;
+                MessageBox.Show("Возникла ошибка соединения, либо не указано количество mac-адресов",
+                    "Ошибка",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            finally
+            {
+                cnn.Close();
+            }
+        }
+    }
+}
---
+++ CIS_Lab4/MacGenerator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CIS_Lab4
+{
+    class MacGenerator
+    {
+        public static Random rnd = new Random();
+        public static PhysicalAddress GenerateAddress()
+        {
+            int octet;
+            string stringOctet;
+            PhysicalAddress address;
+            string stringAddress = "";
+
+            octet = rnd.Next(8,256);
+            stringOctet = Convert.ToString(octet, 2);
+            stringOctet = stringOctet.Remove(stringOctet.Length-2, 2);
+            stringOctet.Insert(stringOctet.Length, "00");
+            octet = Convert.ToInt32(stringOctet,2);
+            stringAddress += octet.ToString("X2");
+
+            for (int i = 0; i < 5; i++)
+            {
+                octet = rnd.Next(256);
+                stringAddress += "-" + octet.ToString("X2");
+            }
+
+            address = PhysicalAddress.Parse(stringAddress);
+            //MessageBox.Show(stringAddress);
+            return address;
+        }
+
+        public static string GenerateFirstOctet(string mask)
+        {
+            int octet;
+            string stringOctet;
+            var finalResult = new StringBuilder(mask);
+
+            octet = rnd.Next(8, 256);
+            stringOctet = Convert.ToString(octet, 2);
+            stringOctet = stringOctet.Remove(stringOctet.Length - 2, 2);
+            stringOctet.Insert(stringOctet.Length, "00");
+            octet = Convert.ToInt32(stringOctet, 2);
+
+            finalResult.Remove(0, 2);
+            finalResult.Insert(0, octet.ToString("X2"));
+
+            return finalResult.ToString();
+        }
+    }
+}
