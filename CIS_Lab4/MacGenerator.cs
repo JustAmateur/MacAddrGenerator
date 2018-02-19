1f39649d3e175038c6e066770a386d277dd404d9
Vyacheslav Kuskov <vyacheslav@MacBook-Pro-Vyacheslav.local>
2018-02-19 23:50:03 +0300

    Created methon for generating octet

--- CIS_Lab4/MacGenerator.cs
+++ CIS_Lab4/MacGenerator.cs
@@ -27,6 +27,36 @@ namespace CIS_Lab4
         /// </summary>
         /// <returns>Returns <see cref="PhysicalAddress"/> object with generated value.></returns>
         public static PhysicalAddress GenerateAddress() {
+            
+            var stringAddress = GenerateOctet();
+            int addressOctet;
+
+            // Generating last 5 octets excepting first
+            for (var i = 0; i < 5; i++)
+            {
+                addressOctet = Random.Next(UpperOctetTreshold);
+                stringAddress += $"-{addressOctet:X2}";
+            }
+
+            return PhysicalAddress.Parse(stringAddress);
+        }
+
+        /// <summary>
+        /// Generates first octet of mac address represented by string value.
+        /// </summary>
+        /// <param name="mask">Mac address mask for generating target.</param>
+        /// <returns>Returns <see cref="string"/> value with mask for mac address with first generated octet.</returns>
+        public static string GenerateFirstOctetInMask(string mask)
+        {
+            var finalResult = new StringBuilder(mask);
+            finalResult.Remove(0, 2);
+            finalResult.Insert(0, GenerateOctet());
+
+            return finalResult.ToString();
+        }
+
+        static string GenerateOctet()
+        {
             // Setting address octet range.
             var addressOctet = Random.Next(LowerOctetTreshold, UpperOctetTreshold);
 
@@ -44,42 +74,7 @@ namespace CIS_Lab4
             // Filling address string
             var stringAddress = string.Empty;
             stringAddress += addressOctet.ToString("X2");
-
-            // Generating last 5 octets excepting first
-            for (var i = 0; i < 5; i++)
-            {
-                addressOctet = Random.Next(UpperOctetTreshold);
-                stringAddress += $"-{addressOctet:X2}";
-            }
-
-            return PhysicalAddress.Parse(stringAddress);
-        }
-
-        /// <summary>
-        /// Generates first octet of mac address represented by string value.
-        /// </summary>
-        /// <param name="mask">Mac address mask for generating target.</param>
-        /// <returns>Returns <see cref="string"/> value with mask for mac address with first generated octet.</returns>
-        public static string GenerateFirstOctet(string mask)
-        {
-            // Setting address octet range.
-            var addressOctet = Random.Next(LowerOctetTreshold, UpperOctetTreshold);
-
-            /*
-             * Here we taking string representation,
-             * converting octet to binary and setting it to global.
-             */
-            var stringOctet = Convert.ToString(addressOctet, toBase:2);
-            stringOctet = stringOctet.Remove(stringOctet.Length - 2, 2);
-            stringOctet = stringOctet.Insert(stringOctet.Length, "00");
-
-            addressOctet = Convert.ToInt32(stringOctet, fromBase:2);
-
-            var finalResult = new StringBuilder(mask);
-            finalResult.Remove(0, 2);
-            finalResult.Insert(0, addressOctet.ToString("X2"));
-
-            return finalResult.ToString();
+            return stringAddress;
         }
     }
 }
